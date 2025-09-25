44a4f1f77a8c4fee8139c0730adba1d454622fb5
Guilherme Melos Vieira <guilherme.melos@edu.pucrs.br>
2025-09-24 22:15:47 -0300

    spring init

---
+++ app-cadastro/src/test/java/com/sarc/app_cadastro/AppCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarc.app_cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
